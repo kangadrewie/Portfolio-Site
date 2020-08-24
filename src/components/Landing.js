539b368af039fb5cdd185d0a3aa68d9796bde382
Andrew Gorman <andrewgorman101@gmail.com>
2020-08-24 18:26:39 +0100

    revert react-mic commit

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Provider, Heading, Subhead } from 'rebass';
+import { Hero, CallToAction, ScrollDownIndicator } from 'react-landing-page';
+
+class Landing extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Landing;
